98c941794c431e1da709d9d095b81c2ccd88eb56
rjt <rjt.rockx@gmail.com>
2019-04-24 02:29:41 +0530

    Refactor and cleanup the client
    - changed classname to avoid confusion with the bot clients
    - improved documentation
    - added support for waiting for ratelimits
    - added a validator for ids
    - renamed variables and methods to camelCase

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,114 +1,196 @@
-const request = require('request');
+const got = require('got');
 const User = require('./structures/User');
 
-class Client {
+class UnbelievaboatAPIClient {
     /**
      *
      * @param {string} token - API Token. Get yours from https://unbelievable.pizza/api/docs
      * @param {?object} [options] - Options
      * @param {?string} [options.baseURL] - API hostname. Defaults to https://unbelievable.pizza/api
      * @param {?number} [options.version] - API version. Defaults to the latest version
+	 * @param {?boolean} [options.waitForRatelimit] - Wait for ratelimits in API calls.
      */
-    constructor(token, options = {}) {
-        if (!token) throw new Error('The API token must be specified');
-        this.token = token;
-        this.baseURL = options.baseURL ? options.baseURL : 'https://unbelievable.pizza/api';
-        this.version = options.version !== undefined ? 'v' + options.version : '';
-    }
+	constructor(token, options = {}) {
+		if (!token)
+			throw new Error('The API token must be specified');
+
+		/**
+		 * Token used to authorize API calls.
+		 * @type {string}
+		 */
+		this.token = String(token);
+
+		/**
+		 * API version accessed by the client.
+		 * @type {number}
+		 */
+		this.version = Number(options.version) || 1;
+
+		/**
+		 * Base URL for requests made to the API.
+		 * @type {string}
+		 */
+		this.baseURL = options.baseURL ? String(options.baseURL) : `https://unbelievable.pizza/api/v${this.version}`;
+
+		/**
+		 * Option to wait for ratelimits while making requests.
+		 * @type {boolean}
+		 */
+		this.waitForRatelimit = Boolean(options.waitForRatelimit);
+	}
+
+	/**
+	 * Sleep for a given amount of time.
+	 * @param {number} ms Milliseconds to sleep for
+	 * @returns Resolved promise after sleeping for the given time
+	 * @private
+	 */
+	async _sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
+
+	/**
+	 * Check if a given ID is valid.
+	 * Discord IDs are usually in the range of 17 to 18 digits.
+	 * To be safe, we use a range of 15 to 20 digits.
+	 * Numeric IDs can cause loss of precision so we utilise strings.
+	 * @param {string} id ID to validate
+	 * @returns {boolean} Whether the ID is valid or not
+	 * @private
+	 */
+	_isValidId(id) {
+		return typeof id === "string" && /^\d{15,20}$/m.test(id);
+	}
 
     /**
-     * Get a user balance
-     * @param guild_id - Guild ID
-     * @param user_id - User ID
-     * @returns {Promise<User>}
+     * Get the balance of a user.
+     * @param {string} guildId Guild ID
+     * @param {string} userId User ID
+     * @returns {User} User object.
      */
-    getUserBalance(guild_id, user_id) {
-        if (!guild_id) throw new Error('guild_id must be specified');
-        if (!user_id) throw new Error('user_id must be specified');
-        return this._request('GET', `guilds/${guild_id}/users/${user_id}`)
-            .then(data => new User(data));
-    }
+	async getUserBalance(guildId, userId) {
+		if (!guildId)
+			throw new Error('guildId must be specified.');
+		if (!this._isValidId(guildId))
+			throw new Error('Invalid guildId specified.');
+		if (!userId)
+			throw new Error('userId must be specified.');
+		if (!this._isValidId(userId))
+			throw new Error('Invalid userId specified.');
+		return this._request('GET', `guilds/${guildId}/users/${userId}`)
+			.then(data => new User(data));
+	}
 
 
     /**
      * Set a user's balance to the given params
-     * @param {string} guild_id - Guild ID
-     * @param {string} user_id - User ID
+     * @param {string} guildId - Guild ID
+     * @param {string} userId - User ID
      * @param {number|string} [cash] Value to set the cash balance to
      * @param {number|string} [bank] Value to set the bank balance to
      * @param {string} [reason] Reason for the audit log
-     * @returns {Promise<User>}
+     * @returns {Promise<User>} User object.
      */
-    setUserBalance(guild_id, user_id, { cash, bank } = {}, reason) {
-        if (!guild_id) throw new Error('guild_id must be specified');
-        if (!user_id) throw new Error('user_id must be specified');
-        if (!cash && !bank) throw new Error('cash or bank must be specified');
-        if (cash === Infinity) cash = 'Infinity';
-        if (bank === Infinity) bank = 'Infinity';
-        return this._request('PUT', `guilds/${guild_id}/users/${user_id}`, { cash, bank, reason })
-            .then(data => new User(data));
-    }
+	async setUserBalance(guildId, userId, { cash, bank } = {}, reason = '') {
+		if (!guildId)
+			throw new Error('guildId must be specified.');
+		if (!this._isValidId(guildId))
+			throw new Error('Invalid guildId specified.');
+		if (!userId)
+			throw new Error('userId must be specified.');
+		if (!this._isValidId(userId))
+			throw new Error('Invalid userId specified.');
+		if (!cash && !bank)
+			throw new Error('cash or bank must be specified');
+		if (cash === Infinity)
+			cash = 'Infinity';
+		if (bank === Infinity)
+			bank = 'Infinity';
+		return this._request('PUT', `guilds/${guildId}/users/${userId}`, { cash, bank, reason })
+			.then(data => new User(data));
+	}
 
     /**
-     * Increase or decrease the User's balance by the cash and bank values given.
-     * Use a negative number to decrease.
-     * @param {string} guild_id - Guild ID
-     * @param {string} user_id - User ID
+     * Increase or decrease a user's balance by the cash and bank values given.
+     * Use negative values to decrease.
+     * @param {string} guildId - Guild ID
+     * @param {string} userId - User ID
      * @param {number|string} [cash] Value to increase/decrease the cash balance by
      * @param {number|string} [bank] Value to increase/decrease the bank balance by
      * @param {string} [reason] Reason for the audit log
-     * @returns {Promise<User>}
+     * @returns {Promise<User>} User object.
      */
-    editUserBalance(guild_id, user_id, { cash, bank } = {}, reason) {
-        if (!guild_id) throw new Error('guild_id must be specified');
-        if (!user_id) throw new Error('user_id must be specified');
-        if (!cash && !bank) throw new Error('cash or bank must be specified');
-        if (cash === Infinity) cash = 'Infinity';
-        if (bank === Infinity) bank = 'Infinity';
-        return this._request('PATCH', `guilds/${guild_id}/users/${user_id}`, { cash, bank, reason })
-            .then(data => new User(data));
-    }
+	async editUserBalance(guildId, userId, { cash, bank } = {}, reason = '') {
+		if (!guildId)
+			throw new Error('guildId must be specified.');
+		if (!this._isValidId(guildId))
+			throw new Error('Invalid guildId specified.');
+		if (!userId)
+			throw new Error('userId must be specified.');
+		if (!this._isValidId(userId))
+			throw new Error('Invalid userId specified.');
+		if (!cash && !bank)
+			throw new Error('cash or bank must be specified');
+		if (cash === Infinity)
+			cash = 'Infinity';
+		if (bank === Infinity)
+			bank = 'Infinity';
+		return this._request('PATCH', `guilds/${guildId}/users/${userId}`, { cash, bank, reason })
+			.then(data => new User(data));
+	}
 
 
     /**
-     * Get a guild leaderboard
-     * @param guild_id - Guild ID
-     * @returns {Promise<Array<User>>}
+     * Get the leaderboard of a particular guild.
+     * @param guildId - Guild ID
+     * @returns {Promise<Array<User>>} Leaderboard of the guild.
      */
-    getGuildLeaderboard(guild_id) {
-        if (!guild_id) throw new Error('guild_id must be specified');
-        return this._request('GET', `guilds/${guild_id}/users`)
-            .then(data => data.map(user => new User(user)));
-    }
+	async getGuildLeaderboard(guildId) {
+		if (!guildId)
+			throw new Error('guildId must be specified.');
+		if (!this._isValidId(guildId))
+			throw new Error('Invalid guildId specified.');
+		return this._request('GET', `guilds/${guildId}/users`)
+			.then(data => data.map(user => new User(user)));
+	}
 
 
     /**
-     * Send a request
+     * Send a request to the API.
      * @param method - HTTP method (GET, PUT, PATCH etc.)
      * @param endpoint - API endpoint
      * @param [data] - JSON data
-     * @returns {Promise<Object>}
+     * @returns {Promise<Object>} Parsed response from the API
      * @private
      */
-    _request(method, endpoint, data = {}) {
-        return new Promise((resolve, reject) => {
-            const options = {
-                headers: { Authorization: this.token, 'Content-Type': 'application/json' },
-                uri: `${this.baseURL}/${this.version ? `${this.version} /` : ''}${endpoint}`,
-                method: method,
-                json: data
-            };
+	async _request(method, endpoint, data = {}) {
+		let response = null;
 
-            request(options, (err, res, body) => {
-                if (err) return reject(err);
-                if (res.statusCode === 200) {
-                    resolve(body);
-                } else {
-                    reject(new Error(body && body.error ? body.error + ' - ' + body.message : `${res.statusCode}: ${res.statusMessage}`));
-                }
-            });
-        });
-    }
+		// use a loop to enable subsequent requests if the first one gets ratelimited
+		while (!response) {
+			response = await got(endpoint, {
+				baseUrl: this.baseURL,
+				headers: { Authorization: this.token },
+				json: data, method, retry: 0, throwHttpErrors: false
+			});
+
+			if (response.statusCode === 200) {
+				console.count("success");
+				return response.body;
+			}
+
+			else if (response.statusCode === 429 && this.waitForRatelimit) {
+				await this._sleep(response.body.retry_after);
+				response = null;
+				continue;
+			}
+
+			else if (response.body & response.body.error)
+				throw new Error(`${response.body.error} - ${response.body.message}`);
+
+			else throw new Error(`${response.statusCode} - ${response.statusMessage}`);
+		}
+	}
 }
 
-module.exports = Client;
+module.exports = UnbelievaboatAPIClient;
