93fe03bfa1a8a3db7479a52e29c478aad37ea6e0
BannerBomb <BannerBomb55@gmail.com>
2020-11-17 22:16:24 -0500

    Fixed indention

--- src/lib/Client.js
+++ src/lib/Client.js
@@ -2,7 +2,7 @@ const { Guild, Permission, User } = require('./structures');
 const RequestHandler = require('./RequestHandler');
 
 class Client {
-    /**
+	/**
 	 *
 	 * @constructs Client
 	 * @param {string} token - Your api token. You may get one from https://unbelievaboat.com/api/docs.
@@ -12,21 +12,21 @@ class Client {
 	 * @param {number} [options.maxRetries=3] - The maximum number of times to retry a request if it's ratelimited. Defaults to 3.
 	 * @throws {TypeError}
 	 */
-    constructor(token, options = {}) {
-        if (typeof token !== 'string') throw new TypeError('The API token must be a string');
-		if (typeof options !== 'object') throw new TypeError('options must be an object');
-		if (options.baseURL !== undefined && typeof options.baseURL !== 'string') throw new TypeError('baseURL must be a string');
-		if (options.version !== undefined && typeof options.version !== 'number') throw new TypeError('version must be a number');
-		if (options.maxRetries !== undefined && typeof options.maxRetries !== 'number') throw new TypeError('maxRetries must be a number');
+	constructor(token, options = {}) {
+		if (typeof token !== 'string') throw new TypeError('The API token must be a string');
+	if (typeof options !== 'object') throw new TypeError('options must be an object');
+	if (options.baseURL !== undefined && typeof options.baseURL !== 'string') throw new TypeError('baseURL must be a string');
+	if (options.version !== undefined && typeof options.version !== 'number') throw new TypeError('version must be a number');
+	if (options.maxRetries !== undefined && typeof options.maxRetries !== 'number') throw new TypeError('maxRetries must be a number');
 
-        this.token = token;
-        this.baseURL = options.baseURL ? options.baseURL : 'https://unbelievaboat.com/api';
-        this.version = options.version !== undefined ? 'v' + options.version : '';
-        this.maxRetries = options.maxRetries || 3;
-        this.requestHandler = new RequestHandler(this);
-    }
+		this.token = token;
+		this.baseURL = options.baseURL ? options.baseURL : 'https://unbelievaboat.com/api';
+		this.version = options.version !== undefined ? 'v' + options.version : '';
+		this.maxRetries = options.maxRetries || 3;
+		this.requestHandler = new RequestHandler(this);
+	}
 
-    /**
+	/**
 	 * Get a users balance.
 	 * 
 	 * @public
@@ -37,15 +37,15 @@ class Client {
 	 * 
 	 * @see {@link https://unbelievaboat.com/api/docs#get-user-balance|Get User Balance}
 	 */
-    getUserBalance(guildId, userId) {
-        if (typeof guildId !== 'string') throw new TypeError('guildId must be a string');
-		if (typeof userId !== 'string') throw new TypeError('userId must be a string');
+	getUserBalance(guildId, userId) {
+		if (typeof guildId !== 'string') throw new TypeError('guildId must be a string');
+	if (typeof userId !== 'string') throw new TypeError('userId must be a string');
 
-        return this._request('GET', `guilds/${guildId}/users/${userId}`)
-            .then(data => new User(data));
-    }
+		return this._request('GET', `guilds/${guildId}/users/${userId}`)
+			.then(data => new User(data));
+	}
 
-    /**
+	/**
 	 * Set a users balance to the provided values.
 	 * 
 	 * @public
@@ -60,8 +60,8 @@ class Client {
 	 * 
 	 * @see {@link https://unbelievaboat.com/api/docs#put-user-balance|Put User Balance}
 	 */
-    setUserBalance(guildId, userId, data = {}, reason) {
-        if (typeof guildId !== 'string') throw new TypeError('guildId must be a string');
+	setUserBalance(guildId, userId, data = {}, reason) {
+		if (typeof guildId !== 'string') throw new TypeError('guildId must be a string');
 		if (typeof userId !== 'string') throw new TypeError('userId must be a string');
 		if (typeof data !== 'object' || (data.cash === undefined && data.bank === undefined)) throw new Error('either cash or bank must be specified');
 		if (data.cash !== undefined && ((typeof data.cash !== 'number' && typeof data.cash !== 'string') || isNaN(data.cash))) throw new TypeError('cash must be a string or number');
@@ -69,11 +69,11 @@ class Client {
 		if (data.cash === Infinity) data.cash = 'Infinity';
 		if (data.bank === Infinity) data.bank = 'Infinity';
 
-        return this._request('PUT', `guilds/${guildId}/users/${userId}`, { cash: data.cash, bank: data.bank, reason })
-            .then(data => new User(data));
-    }
+		return this._request('PUT', `guilds/${guildId}/users/${userId}`, { cash: data.cash, bank: data.bank, reason })
+			.then(data => new User(data));
+	}
 
-    /**
+	/**
 	 * Increase or decrease a users balance by the cash and bank values given.
 	 * Use negative values to decrease.
 	 * 
@@ -89,8 +89,8 @@ class Client {
 	 * 
 	 * @see {@link https://unbelievaboat.com/api/docs#patch-user-balance|Patch User Balance}
 	 */
-    editUserBalance(guildId, userId, data = {}, reason) {
-        if (typeof guildId !== 'string') throw new TypeError('guildId must be a string');
+	editUserBalance(guildId, userId, data = {}, reason) {
+		if (typeof guildId !== 'string') throw new TypeError('guildId must be a string');
 		if (typeof userId !== 'string') throw new TypeError('userId must be a string');
 		if (typeof data !== 'object' || (data.cash === undefined && data.bank === undefined)) throw new Error('either cash or bank must be specified');
 		if (data.cash !== undefined && ((typeof data.cash !== 'number' && typeof data.cash !== 'string') || isNaN(data.cash))) throw new TypeError('cash must be a string or number');
@@ -98,11 +98,11 @@ class Client {
 		if (data.cash === Infinity) data.cash = 'Infinity';
 		if (data.bank === Infinity) data.bank = 'Infinity';
 
-        return this._request('PATCH', `guilds/${guildId}/users/${userId}`, { cash: data.cash, bank: data.bank, reason })
-            .then(data => new User(data));
-    }
+		return this._request('PATCH', `guilds/${guildId}/users/${userId}`, { cash: data.cash, bank: data.bank, reason })
+			.then(data => new User(data));
+	}
 
-    /**
+	/**
 	 * Get the leaderboard for a guild.
 	 * 
 	 * @public
@@ -117,29 +117,28 @@ class Client {
 	 * 
 	 * @see {@link https://unbelievaboat.com/api/docs#get-guild-leaderboard|Get Guild Leaderboard}
 	 */
-    getGuildLeaderboard(guildId, query = {}) {
-        if (typeof guildId !== 'string') throw new TypeError('guildId must be a string');
+	getGuildLeaderboard(guildId, query = {}) {
+		if (typeof guildId !== 'string') throw new TypeError('guildId must be a string');
 		if (typeof query !== 'object') throw new TypeError('query must be an object');
 		if (query.sort !== undefined && !['total', 'cash', 'bank'].includes(query.sort)) throw new TypeError('sort must be one of total, cash or bank');
 		if (query.limit !== undefined && typeof query.limit !== 'number') throw new TypeError('limit must be a number');
 		if (query.offset !== undefined && typeof query.offset !== 'number') throw new TypeError('offset must be a number');
 		if (query.page !== undefined && typeof query.page !== 'number') throw new TypeError('page must be a number');
 
-        return this._request('GET', `guilds/${guildId}/users`, {}, query)
-            .then(data => {
-                if (data.users) {
-                    return {
-                        users: data.users.map(user => new User(user)),
-                        totalPages: data.total_pages
-                    };
-                } else {
-                    return data.map(user => new User(user));
-                }
-            });
-    }
+		return this._request('GET', `guilds/${guildId}/users`, {}, query)
+			.then(data => {
+				if (data.users) {
+					return {
+						users: data.users.map(user => new User(user)),
+						totalPages: data.total_pages
+					};
+				} else {
+					return data.map(user => new User(user));
+				}
+			});
+	}
 
-    /**
-    /**
+	/**
 	 * Retrieve basic information for a guild.
 	 * 
 	 * @public
@@ -149,14 +148,14 @@ class Client {
 	 * 
 	 * @see {@link https://unbelievaboat.com/api/docs#get-guild|Get Guild}
 	 */
-    getGuild(guildId) {
-        if (typeof guildId !== 'string') throw new TypeError('guildId must be a string');
+	getGuild(guildId) {
+		if (typeof guildId !== 'string') throw new TypeError('guildId must be a string');
 
-        return this._request('GET', `guilds/${guildId}`)
-            .then(data => new Guild(data));
-    }
+		return this._request('GET', `guilds/${guildId}`)
+			.then(data => new Guild(data));
+	}
 
-    /**
+	/**
 	 * Retrieve the permissions for the application.
 	 * This can only be used with an application API token.
 	 * 
@@ -167,14 +166,14 @@ class Client {
 	 * 
 	 * @see {@link https://unbelievaboat.com/api/docs#get-application-permissions|Get Application Permissions}
 	 */
-    getApplicationPermission(guildId) {
-        if (typeof guildId !== 'string') throw new TypeError('guildId must be a string');
+	getApplicationPermission(guildId) {
+		if (typeof guildId !== 'string') throw new TypeError('guildId must be a string');
 
-        return this._request('GET', `applications/@me/guilds/${guildId}`)
-            .then(data => new Permission(data.permissions));
-    }
+		return this._request('GET', `applications/@me/guilds/${guildId}`)
+			.then(data => new Permission(data.permissions));
+	}
 
-    /**
+	/**
 	 * Send a request.
 	 * 
 	 * @private
@@ -184,9 +183,9 @@ class Client {
 	 * @param {object} [query={}] - The query string options to use in the url.
 	 * @returns {Promise<any>} The raw request data.
 	 */
-    _request(method, endpoint, data = {}, query = {}) {
-        return this.requestHandler.request(method, endpoint, data, query);
-    }
+	_request(method, endpoint, data = {}, query = {}) {
+		return this.requestHandler.request(method, endpoint, data, query);
+	}
 }
 
 module.exports = Client;
